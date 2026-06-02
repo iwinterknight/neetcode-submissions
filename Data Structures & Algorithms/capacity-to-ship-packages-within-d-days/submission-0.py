dd308346815f0adea9c6e788d23f4c5aecf2e785
Sunit <37212007+iwinterknight@users.noreply.github.com>
2026-06-02 17:09:53 -0400

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def calculate_days_to_ship(capacity):
+            days_to_ship = 0
+            residual_capacity = capacity
+            i = 0
+            while i < len(weights):
+                # print(days, i, weights[i], residual_capacity)
+                if weights[i] <= residual_capacity:
+                    residual_capacity -= weights[i]
+                    i += 1
+                else:
+                    days_to_ship += 1
+                    residual_capacity = capacity
+            return days_to_ship + 1
+
+        def bisect_right():
+            # greatest element lte
+            l, r = max(weights), sum(weights)
+            while l <= r:
+                mid = l + (r-l) // 2
+                days_to_ship = calculate_days_to_ship(mid)
+                if days_to_ship <= days:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return l
+
+        return bisect_right()
+
+                
